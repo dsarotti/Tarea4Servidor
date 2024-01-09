5c3d2329d9af98e8fa3502d490f6bb79cc396b2b
mirianbeta <miyibeta1413@gmail.com>
2024-01-09 12:49:51 +0100

    Asignatura implementada con el constructor, getters y setters.

---
+++ src/main/java/org/servidor/Asignatura.java
@@ -0,0 +1,29 @@
+package org.servidor;
+
+import java.io.Serializable;
+
+public class Asignatura implements Serializable {
+    private int id;
+    private String nombreasig;
+
+    public Asignatura(int id, String nombreasig) {
+        this.id = id;
+        this.nombreasig = nombreasig;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreasig() {
+        return nombreasig;
+    }
+
+    public void setNombreasig(String nombreasig) {
+        this.nombreasig = nombreasig;
+    }
+}
