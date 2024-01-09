692cbd6be330ce0c89ae27794a39ed35b25e3e1c
mirianbeta <miyibeta1413@gmail.com>
2024-01-09 13:14:11 +0100

    Clase Especialidad impementada con su constructor, getters y setters.

---
+++ src/main/java/org/servidor/Especialidad.java
@@ -0,0 +1,29 @@
+package org.servidor;
+
+import java.io.Serializable;
+
+public class Especialidad implements Serializable {
+    private int id;
+    private String nombreEspecialidad;
+
+    public Especialidad(int id, String nombreespe) {
+        this.id = id;
+        this.nombreEspecialidad = nombreEspecialidad;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNombreEspecialidad(String nombreEspecialidad) {
+        this.nombreEspecialidad = nombreEspecialidad;
+    }
+
+    public String getNombreEspecialidad() {
+        return nombreEspecialidad;
+    }
+}
