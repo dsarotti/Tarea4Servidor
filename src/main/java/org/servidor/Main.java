242e0868e8014e0f297eca218d113ae5c1f606e9
dsarotti <sr.sarotti@gmail.com>
2024-01-12 00:25:12 +0100

    método init() inicializa los profesores en la clase Servidor
    método report() muestra toda la información de los profesores.

---
+++ src/main/java/org/servidor/Main.java
@@ -0,0 +1,10 @@
+package org.servidor;
+
+public class Main {
+    public static void main(String[] args) {
+        Servidor servidor = new Servidor();
+        servidor.init();
+        servidor.report();
+
+    }
+}
--- src/main/java/org/servidor/Servidor.java
+++ src/main/java/org/servidor/Servidor.java
@@ -2,41 +2,61 @@ package org.servidor;
 public class Servidor
 {
     private Profesor[] profesores;
-    private int lastId;
+    private int lastIdProfesor;
+    private int lastIdEspecialidad;
+    private int lastIdAsignatura;
 
     public Servidor(){
-        this.profesores =new Profesor[5];
-        this.lastId=0;
-        init();
-
-        for(int i = 0;i< profesores.length;i++){
-            System.out.println("idProfesor: "+profesores[i].getIdprofesor());
-            System.out.println("\n NombreProfesor:" + profesores[i].getNombre());
-
-        }
+        this.lastIdProfesor =0;
+        this.lastIdAsignatura=300;
+        this.lastIdEspecialidad=200;
     }
 
-    private void init(){
+    /**
+     * Inicializa el array de 5 profesores con una especialidad y 3 asignaturas cada uno.
+     */
+    public void init(){
+        profesores=new Profesor[5];
         //Inicializar profesores
         for(int i=0;i<5;i++){
-            int idTemp = getNewId();
-            Especialidad espe = new Especialidad(idTemp,"especialidad " + idTemp );
+            int idTempEspe = getNewIdEspecialidad();
+            Especialidad espe = new Especialidad(idTempEspe,"especialidad " + idTempEspe );
             String nombreProfesor="Nombre " +i;
             Asignatura[] asignaturas = new Asignatura[3];
-            for (int j = 0 ; i<3;i++){
-                asignaturas[j] = new Asignatura(getNewId(),"NombreAsignatura" + j );
+            for (int j = 0 ; j<3;j++){
+                asignaturas[j] = new Asignatura(getNewIdAsignatura(),"NombreAsignatura" + j );
             }
-            profesores[i]=new Profesor(getNewId(),nombreProfesor,asignaturas,espe);
+            profesores[i]=new Profesor(getNewIdProfesor(),nombreProfesor,asignaturas,espe);
         }
     }
 
-    private int getNewId(){
-        return lastId++;
-    }
+    private int getNewIdEspecialidad(){return lastIdEspecialidad++;}
 
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private int getNewIdProfesor(){return lastIdProfesor++;}
+
+    private int getNewIdAsignatura(){return lastIdAsignatura++;}
+
+    //Muestra por la salida estándar los datos de los profesores y sus asignaturas. (para debug)
+    public void report(){
+        StringBuilder builder = new StringBuilder();
+        for (Profesor profesor : profesores) {
+            builder.setLength(0);
+            System.out.println(builder.append("Id: ").append(profesor.getIdprofesor()));
+            builder.setLength(0);
+            System.out.println(builder.append("Nombre: ").append(profesor.getNombre()));
+
+            System.out.println("Especialidad: ");
+            builder.setLength(0);
+            System.out.print((builder.append("Id: ").append(profesor.getEspecialidad().getId())).toString().indent(4));
+            builder.setLength(0);
+            System.out.print((builder.append("Nombre: ").append(profesor.getEspecialidad().getNombreEspecialidad())).toString().indent(4));
+            System.out.println("Asignaturas: ");
+            for (int j = 0; j < profesor.getAsignaturas().length; j++) {
+                builder.setLength(0);
+                System.out.print(builder.append("Id: ").append(profesor.getAsignaturas()[j].getId()).toString().indent(4) );
+                builder.setLength(0);
+                System.out.print(builder.append("Nombre: ").append(profesor.getAsignaturas()[j].getNombreAsignatura()).toString().indent(4));
+            }
+        }
     }
 }
-
