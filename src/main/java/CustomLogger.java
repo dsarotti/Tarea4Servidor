d0542940f9b509cb86a9b083a595fddb8d1c84f1
mirianbeta <miyibeta1413@gmail.com>
2024-01-15 00:03:53 +0100

    Implementación de CustomLogger. Se añadieron logs a las conexiones, desconexiones y solicitudes de clientes. Se añadió FichLog.txt a .gitignore.

---
+++ src/main/java/CustomLogger.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+public class CustomLogger {
+
+    static CustomLogger customLoggerInstance;
+    private File logFile;
+
+    private static File inicializarCustomLogger() {
+        // Nombre del archivo a verificar/crear
+        String nombreArchivo = "FichLog.txt";
+
+        // Verificar si el archivo existe
+        File archivo = new File(nombreArchivo);
+        if (!archivo.exists()) {
+            try {
+                // Crear el archivo si no existe
+                archivo.createNewFile();
+                System.out.println("Se ha creado el archivo de log " + nombreArchivo);
+            } catch (IOException e) {
+                System.err.println("Error al crear el archivo de log " + nombreArchivo);
+                e.printStackTrace();
+            }
+        }
+        return archivo;
+    }
+
+    public void agregarLineaAlFinal(String nuevaLinea) {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(logFile, true));
+
+            // Usamos true como segundo parámetro para indicar que queremos abrir el archivo en modo "append"
+            // Es decir, que se añadan nuevas líneas al final del archivo
+
+            // Escribimos la nueva línea al final del archivo
+            writer.write(nuevaLinea );
+            writer.newLine(); // Añadimos un salto de línea después de la nueva línea
+            writer.flush();
+            System.out.println("Se ha añadido la línea al archivo.");
+
+        } catch (IOException e) {
+            System.err.println("Error al añadir la línea al archivo.");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static String formatearTimestamp(Timestamp timestamp) {
+        // Se crea un objeto SimpleDateFormat con el formato deseado
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        // Se convierte el Timestamp a un objeto Date
+        Date date = new Date(timestamp.getTime());
+
+        // Se formatea la fecha utilizando el objeto SimpleDateFormat
+        return formatoFecha.format(date);
+    }
+
+    private CustomLogger() {
+        logFile = inicializarCustomLogger();
+    }
+
+    public static CustomLogger getInstance() {
+        if (customLoggerInstance == null) {
+            customLoggerInstance = new CustomLogger();
+        }
+        return customLoggerInstance;
+    }
+}
